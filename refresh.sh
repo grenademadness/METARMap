6f40feef01558e8e76a5de8ee9187969f90575fc
grenademadness <125998241+grenademadness@users.noreply.github.com>
2024-03-16 18:47:16 -0400

    Update refresh.sh
    added update.py to the refresh script.

--- refresh.sh
+++ refresh.sh
@@ -1,3 +1,6 @@
 /usr/bin/sudo pkill -F /home/pi/offpid.pid
 /usr/bin/sudo pkill -F /home/pi/metarpid.pid
+echo Checking AWS Bucket Updates
+/usr/bin/sudo /usr/bin/python3 /home/pi/update.py
 /usr/bin/sudo /usr/bin/python3 /home/pi/metar.py & echo $! > /home/pi/metarpid.pid
+echo Restarting
